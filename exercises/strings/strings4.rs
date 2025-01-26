57e6212d75770931bf7fadaab9967cd6f387300c
chao-sun01io <csun01io@outlook.com>
2025-01-26 22:07:28 +0000

    Update

--- exercises/strings/strings4.rs
+++ exercises/strings/strings4.rs
@@ -7,8 +7,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 fn string_slice(arg: &str) {
     println!("{}", arg);
 }
@@ -17,14 +15,20 @@ fn string(arg: String) {
 }
 
 fn main() {
-    ???("blue");
-    ???("red".to_string());
-    ???(String::from("hi"));
-    ???("rust is fun!".to_owned());
-    ???("nice weather".into());
-    ???(format!("Interpolation {}", "Station"));
-    ???(&String::from("abc")[0..1]);
-    ???("  hello there ".trim());
-    ???("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string_slice("blue");
+    // Convert string slice to owned string
+    string("red".to_string());
+    string(String::from("hi"));
+    string("rust is fun!".to_owned());
+
+    // .into() converts string slice to inferred type, it can be either string or string slice
+    string("nice weather".into());
+    string_slice("nice weather".into());
+    
+    // format!() creates a new string
+    string(format!("Interpolation {}", "Station"));
+    string_slice(&String::from("abc")[0..1]);
+    string_slice("  hello there ".trim());
+    string("Happy Monday!".to_string().replace("Mon", "Tues"));
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase());
 }
